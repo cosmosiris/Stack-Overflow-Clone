399d09e8455c87e151073942d43d24a3edbbcf0b
Iris Nevins  & Youssef Kholeif <pair+cosmosiris.yykholeif@devbootcamp.com>
2017-10-19 12:26:10 -0700

    Create routes and login helper methods

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,9 @@
+get '/sessions/:id' do
+  erb :index
+end
+
+get '/sessions/new' do
+  erb :new
+end
+
+post  ''
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+get '/users/new' do
+  erb :'users/new'
+end
+
+
+
+
+post '/users' do
+  @user = User.new(params[:user])
+  @confirmed = params[:user][:password] == params[:password_confirmation]
+  if @confirmed && @user.save
+    login(@user)
+    redirect "/sessions/#{session.id}"
+  else
+    @errors = @user.errors.full_messages
+    @errors << "you shall not pass" unless @confirmed
+    erb :'users/new'
+  end
+end
